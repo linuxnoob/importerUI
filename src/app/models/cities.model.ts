83cb86f5257da5437f127c0d25ccd64fbb0ff205
gburnama <blackprimenoz@gmail.com>
2022-01-10 18:50:39 +0700

    first commit

---
+++ src/app/models/cities.model.ts
@@ -0,0 +1,13 @@
+export class Cities{
+    cityId?: number;
+    cityName? : string;
+    stateProvinceId? : number;
+    location?: string;
+    latestRecordedPopulation?: number;
+    lastEditedBy?: number;
+    validFrom?: any;
+    validTo?: any;
+    
+
+
+}
